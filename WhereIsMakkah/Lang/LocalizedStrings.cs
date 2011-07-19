31a10bcd9d62ef4b94a29d8f6fcd89ae164f406b
Ron Huang <ronhuang@gmail.com>
2011-07-19 23:40:45 +0800

    Added License pivot in settings. Also created AppResources for localization.

---
+++ WhereIsMakkah/Lang/LocalizedStrings.cs
@@ -0,0 +1,19 @@
+﻿namespace WhereIsMakkah.Lang
+{
+    public class LocalizedStrings
+    {
+        public LocalizedStrings()
+        {
+        }
+
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources
+        {
+            get
+            {
+                return _localizedResources;
+            }
+        }
+    }
+}
--- WhereIsMakkah/Properties/AssemblyInfo.cs
+++ WhereIsMakkah/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -10,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ron Huang")]
 [assembly: AssemblyProduct("Where Is Makkah")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright © Ron Huang 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,3 +34,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("0.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
