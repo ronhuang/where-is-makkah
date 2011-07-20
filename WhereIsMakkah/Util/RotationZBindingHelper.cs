50c4be28f98e312bb2ab05cdf8982c5c88741cb6
Ron Huang <ronhuang@gmail.com>
2011-07-20 18:00:24 +0800

    Replaced features of WP7.1 with WP7. BlockArrow -> Polygon.

---
+++ WhereIsMakkah/Util/RotationZBindingHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WhereIsMakkah.Util
+{
+    public class RotationZBindingHelper
+    {
+        #region RotationZ (Attached DependencyProperty)
+
+        public static readonly DependencyProperty RotationZProperty =
+                DependencyProperty.RegisterAttached("RotationZ",
+                typeof(double),
+                typeof(RotationZBindingHelper),
+                new PropertyMetadata(0.0, new PropertyChangedCallback(OnRotationZChanged)));
+
+        public static void SetRotationZ(DependencyObject o, double value)
+        {
+            o.SetValue(RotationZProperty, value);
+        }
+
+        public static double GetRotationZ(DependencyObject o)
+        {
+            return (double)o.GetValue(RotationZProperty);
+        }
+
+        private static void OnRotationZChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            FrameworkElement element = d as FrameworkElement;
+            if (element != null)
+            {
+                PlaneProjection projection = element.Projection as PlaneProjection;
+                if (projection != null)
+                {
+                    projection.RotationZ = (double)e.NewValue;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
